1fc1ec628900adeaefbd5c4125fb2218ec676757
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-09-19 23:37:23 +0300

    Homework #2-2 - Done

--- src/main/java/HW_IV/Main.java
+++ src/main/java/HW_IV/Main.java
@@ -1,50 +1,51 @@
-//package HW_IV;
-//
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        while(true){
-//
-//            System.out.println("Программа завершена!");
-//        }
-//
-//    }
-//
-//    public static void calculation(){
-//        int earnings = 0;
-//        int spending = 0;
-//
-//        int operation = Integer.parseInt(input);
-//        switch (operation) {
-//            case 1:
-//                System.out.println("Введите сумму дохода:");
-//                String moneyStr = scanner.nextLine();
-//                int money = Integer.parseInt(moneyStr);
-//                earnings += money;
-//                break;
-//            case 2:
-//                // действия при выборе второй операции
-//
-//                break;
-//            case 3:
-//                // действия при выборе третьей операции
-//
-//                break;
-//            default:
-//                System.out.println("Такой операции нет");
-//        }
-//    }
-//    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
-//        int tax = (earnings - spendings) * 15 / 100;
-//        if (tax >= 0) {
-//            return tax;
-//        } else {
-//
-//            return 0;
-//        }
-//    }
-//
-//}
+package HW_IV;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(final String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+        while (true){
+
+            System.out.println("Выберите операцию и введите её номер:\n" +
+                    "1. Добавить новый доход\n" +
+                    "2. Добавить новый расход\n" +
+                    "3. Выбрать систему налогообложения");
+             String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("end")){
+            System.out.println("Всего доброго!");
+            break;}
+
+            int earnings = 0;
+            int spendings = 0;
+
+            int operation = Integer.parseInt(input);
+            switch (operation) {
+                case 1:
+                    System.out.println("Введите сумму дохода:");
+                    String expenseString = scanner.nextLine();
+                    int moneyAdd = Integer.parseInt(expenseString);
+                    earnings += moneyAdd;
+                    break;
+                case 2:
+                    System.out.println("Введите сумму расхода:");
+                    int moneyStr;
+                    break;
+                case 3:
+                    System.out.println("Введите сумму расхода:");
+
+                    break;
+                default:
+                    System.out.println("Такой операции нет");
+            }
+
+        }
+        System.out.println("Программа завершена!");
+
+
+
+    }
+
+}
