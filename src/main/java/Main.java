cb9ec5a789ea9f8e13758e07e85f031fd3725baa
1N73RC3P70R <kras.konstantinov@yandex.ru>
2023-09-23 21:54:11 +0300

    Started HW #7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,12 @@
 import java.util.Random;
 
 public class Main {
+    public static final int SIZE;
     public static void main(String[] args) {
-        int[][] colors = new int[5][5];
+        int[][] colors = new int[SIZE][SIZE];
         Random random = new Random();
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 colors[i][j] = random.nextInt(256);
             }
         }
